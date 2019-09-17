a4d8253da78f1b27e135f315ac98173230de51cf
sarah <s.j.condon01@gmail.com>
2019-09-17 03:13:49 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def badge_maker 
+  names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+  counter = 1
+  names.each do |names|
+    puts "room number #{counter}"
+    puts ""Hello, #{name}! You'll be assigned to room #{counter}!"
+ counter += 1
+ end
