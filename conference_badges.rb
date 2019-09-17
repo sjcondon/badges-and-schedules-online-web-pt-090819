e5a29a491808623c6b68297c3a5d707762c58c14
sarah <s.j.condon01@gmail.com>
2019-09-17 03:12:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,11 @@
 def badge_maker 
   names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-  
+  counter = 1
+  names.each do |names|
+    puts "room number #{counter}"
+    puts ""
   #
   def batch_badge_creator
-    def 
+    def assign_rooms
+      
+      #1-7 rooms
